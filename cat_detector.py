34e3920c37b570ec3fe7ef3b54ae3d8825717afa
nicorajala <rajalanico08@gmail.com>
2026-06-22 19:48:15 +0300

    added object detection from streamed image

---
+++ cat_detector.py
@@ -0,0 +1,58 @@
+import argparse
+import cv2
+import pyttsx3
+import threading
+import time
+from ultralytics import YOLO
+
+cap = cv2.VideoCapture("http://192.168.1.59:5000/video")
+cap.set(3, 640)                     # x res
+cap.set(4, 480)                     # y res
+
+model = YOLO("yolo/yolov8n.pt")
+
+last_spoken = 0
+COOLDOWN = 4
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--no-gui", action="store_true")
+    args = parser.parse_args()
+
+    global last_spoken
+
+    while True:
+        ret, img = cap.read()
+        results = model(img, imgsz=320, verbose=False)
+
+        for box in results[0].boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = float(box.conf[0])
+            cls = int(box.cls[0])
+            label = f"{model.names[cls]} {conf:.2f}"
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, label, (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            now = time.time()
+            # if now - last_spoken > COOLDOWN:
+            #     last_spoken = now
+            #     threading.Thread(target=speak, args=("Put down your damn phone.",), daemon=True).start()
+
+        if args.no_gui:
+            pass
+        else:
+            cv2.imshow('Dis shit', img)
+            if cv2.waitKey(1) == ord('q'):
+                break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
